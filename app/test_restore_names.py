69fec97f0bffb34c6208002f190a15f12bf3957d
unknown <lh3241pon@gmail.com>
2026-07-28 20:17:54 -0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,105 @@
-import pytest
-from app.restore_names import restore_names
+from copy import deepcopy
 
-# write your tests here
+from app import restore_names
+
+
+def test_restore_none_first_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    restore_names.restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+
+def test_restore_missing_first_name() -> None:
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+    restore_names.restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+
+def test_keep_existing_first_name() -> None:
+    users = [
+        {
+            "first_name": "John",
+            "last_name": "Smith",
+            "full_name": "John Smith",
+        }
+    ]
+
+    expected = deepcopy(users)
+
+    restore_names.restore_names(users)
+
+    assert users == expected
+
+
+def test_restore_mixed_users() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Anna",
+            "last_name": "Brown",
+            "full_name": "Anna Brown",
+        },
+    ]
+
+    restore_names.restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+        {
+            "first_name": "Anna",
+            "last_name": "Brown",
+            "full_name": "Anna Brown",
+        },
+    ]
+
+
+def test_empty_users_list() -> None:
+    users = []
+
+    restore_names.restore_names(users)
+
+    assert users == []
